8e7b80afb2ae25babd11c18a8a93ec3515a3c5ef
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-04 00:33:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,8 +27,8 @@ end
 
 def reverse_transfer
   if @status=="pending"
-    sender.balance
-    receiver.balance+amount
+    sender.balance+amount
+    receiver.balance-amount
     @status="reversed"
   end
 end
